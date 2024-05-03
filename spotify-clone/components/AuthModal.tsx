959324f2c63d57fb1d9abfef4d0d76f2d0657328
Luis Eduardo <luisedubritto@gmail.com>
2024-05-02 22:14:54 -0300

    auth modal and functions

--- spotify-clone/components/AuthModal.tsx
+++ spotify-clone/components/AuthModal.tsx
@@ -7,11 +7,11 @@ import {
 import { useRouter } from "next/navigation";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useEffect } from "react";
 
 import useAuthModal from "@/hooks/useAuthModal";
 
 import Modal from "./Modal";
-import { useEffect } from "react";
 
 const AuthModal = () => {
     const supabaseClient = useSupabaseClient();
@@ -20,8 +20,11 @@ const AuthModal = () => {
     const { onClose, isOpen } = useAuthModal();
 
     useEffect(() =>{
-
-    }, []);
+        if (session) {
+            router.refresh();
+            onClose();
+        }
+    }, [session, router, onClose]);
 
     const onChange = (open: boolean) => {
         if (!open) {
