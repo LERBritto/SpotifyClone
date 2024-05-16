663a15a146eab83d1a831b9bd8e0775f8754fa7f
Luis Eduardo <luisedubritto@gmail.com>
2024-05-16 03:47:24 -0300

    List songs display

--- spotify-clone/next.config.mjs
+++ spotify-clone/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            "irauxlwdmgzgyddgngmd.supabase.co"
+        ]
+    }
+};
 
 export default nextConfig;
